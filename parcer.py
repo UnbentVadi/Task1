2fde94dc284fa02cb92b9e06c8c5364e468aa84b
YuraKolyada <yyoorraa@gmail.com>
2016-04-03 22:38:20 +0300

    finish

--- parcer.py
+++ parcer.py
@@ -8,17 +8,17 @@ def parser():
 	f = urlopen(URL)
 	list_html = f.read().decode("utf-8")
 	list_doc = fromstring(list_html)
-	for eleb in list_doc.cssselect(item):
-		a = eleb.cssselect("p")[5:]
+	for elem in list_doc.cssselect(item):
+		a = elem.cssselect("p")[5:]
 		text_site = [content_site.text_content() for content_site in a]
 		myStringSite = ''.join(text_site)
 	return myStringSite
 
 def write_file(string_text):
-	f = open("main.html", "w", encoding="utf-8")
+	file = open("main.html", "w", encoding="utf-8")
 	#with open("main.html", "w", encoding="utf-8") as f:
-	f.write(u"<p>"+string_text+"</p>")
-	f.close() 
+	file.write("<meta charset=\"utf-8\"><p>"+str(string_text)+"</p>")
+	file.close() 
 
 def main():
 	string_text = parser()
