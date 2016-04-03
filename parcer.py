99b8591d9b1178316e111636664122c43083c285
YuraKolyada <yyoorraa@gmail.com>
2016-04-03 19:50:23 +0300

    two commit(parser text)

--- parcer.py
+++ parcer.py
@@ -10,4 +10,18 @@ def parser():
 	list_doc = fromstring(list_html)
 	for eleb in list_doc.cssselect(item):
 		a = eleb.cssselect("p")[5:]
-		print(a)
+		text_site = [content_site.text_content() for content_site in a]
+		myStringSite = ''.join(text_site)
+	return myStringSite
+
+
+
+
+
+
+def main():
+	string_text = parser()
+	write_file(string_text)
+
+if __name__ == "__main__":
+	main()
